ed5b93f13e698243b70e32467c932fe1ff946862
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2023-02-22 14:11:50 -0300

    dependency injection fixes

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,18 +1,14 @@
 import { Router } from "express";
-import Api from "../../../api";
-import { TutorialsService } from "../../../app/tutorials/service";
-import { TutorialsMySQLRepository } from "../../../app/tutorials/repository";
 import tutorialsRoutes from "./tutorials";
-const v1 = Router();
 
-const api = new Api({
-    tutorialsService: new TutorialsService(new TutorialsMySQLRepository()),
-});
+export default function routes(api) {
+    const router = Router();
 
-v1.get("/", (req, res) => {
-    res.send("OK V1");
-});
+    router.get("/", (req, res) => {
+        res.send("OK V1");
+    });
 
-v1.use("/tutorials", tutorialsRoutes(api));
+    router.use("/tutorials", tutorialsRoutes(api));
 
-export default v1;
+    return router;
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
-import v1 from "./api/routes/v1";
+import Api from "./api";
+import { TutorialsService } from "./app/tutorials/service";
+import { TutorialsMySQLRepository } from "./app/tutorials/repository";
+import RoutesV1 from "./api/routes/v1";
+
 const app = express();
 const port = 3000;
 
+// INFO: Here we instantiate all dependencies and inject them into the api
+const api = new Api({
+    tutorialsService: new TutorialsService(new TutorialsMySQLRepository()),
+});
+
 app.use(express.json());
 
-app.use("/api/v1", v1);
+app.use("/api/v1", RoutesV1(api));
 
 app.get("/", (req, res) => {
     res.send("OK");
